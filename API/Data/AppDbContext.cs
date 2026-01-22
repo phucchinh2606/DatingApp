30eaca567ae74794994272fa98445ef344ea9c10
phucchinh2606 <chinhdo266@gmail.com>
2026-01-22 11:22:51 +0700

    first commit

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions options):base(options)
+    {
+
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
