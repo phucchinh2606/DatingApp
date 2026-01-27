a354a3bfbad059e149f4502e9bca2a38e0bd06ec
phucchinh2606 <chinhdo266@gmail.com>
2026-01-27 14:38:39 +0700

    building UI

--- API/Extensions/AppUserExtensions.cs
+++ API/Extensions/AppUserExtensions.cs
@@ -1,8 +1,21 @@
 using System;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
 
 namespace API.Extensions;
 
-public class AppUserExtensions
+public static class AppUserExtensions
 {
-
+    public static UserDto ToDto(this AppUser user,ITokenService tokenService)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            ImageUrl = user.ImageUrl,
+            Token = tokenService.CreateToken(user)
+        };
+    }
 }
